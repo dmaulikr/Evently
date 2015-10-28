953ec29fe716b6d480164b99da411cae136acd13
Vanessa Dyce <vanessadyce@Vanessas-MacBook-Pro.local>
2015-10-28 00:11:06 -0400

    added calendar events with use of EventKit and minor UI clean up

---
+++ ParseStarterProject/CreateEventViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CreateEventViewController.swift
+//  Evently-ios
+//
+//  Created by Vanessa Dyce on 10/27/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class CreateEventViewController: UIViewController {
+    let eventStore = EKEventStore()
+    var calendars: [EKCalendar]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+    
+    @IBAction func makeEvent(sender: AnyObject){
+        let store = EKEventStore()
+        store.requestAccessToEntityType(EKEntityType.Event, completion: {(accessGranted:Bool, error: NSError?) in
+            if accessGranted == true {
+                let event = EKEvent(eventStore: store)
+                event.title = "Event Title"
+                event.startDate = NSDate() //today
+                event.endDate = event.startDate.dateByAddingTimeInterval(60*60) //1 hour long meeting
+                event.calendar = store.defaultCalendarForNewEvents
+                //let err: NSError?
+                do {
+                    //try store.saveEvent(event, span: EKSpan.ThisEvent)
+                    try store.saveEvent(event, span: EKSpan.ThisEvent)
+                    print("Saved Event")
+
+                } catch {
+                    // report error
+                }
+                
+                //self.savedEventId = event.eventIdentifier //save event id to access this particular event later
+            } else {
+                print("Need to get permission")
+            }
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ParseStarterProject/HomeViewController.swift
+++ ParseStarterProject/HomeViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 import Parse
+import EventKit
 
 class HomeViewController: UIViewController, UITextFieldDelegate  {
 
     @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var eventTitle: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orangeColor()
+
         self.userEmail.text = PFUser.currentUser()?.email
 
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        self.view.addGestureRecognizer(tap)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -24,21 +33,78 @@ class HomeViewController: UIViewController, UITextFieldDelegate  {
         // Dispose of any resources that can be recreated.
     }
     
+//    @IBAction func signOut(sender: AnyObject){
+//        
+//     PFUser.logOut()
+//
+//        
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewControllerWithIdentifier("SignUpInViewController") as UIViewController
+//        self.presentViewController(vc, animated: true, completion: nil)
+//    }
+    
     @IBAction func signOut(sender: AnyObject){
+             PFUser.logOut()
         
-     PFUser.logOut()
-
         
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewControllerWithIdentifier("SignUpInViewController") as UIViewController
-        self.presentViewController(vc, animated: true, completion: nil)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewControllerWithIdentifier("SignUpInViewController") as UIViewController
+                self.presentViewController(vc, animated: true, completion: nil)
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
     }
+    
+    func dismissKeyboard(){
+        eventTitle.resignFirstResponder()
+    }
+    
+    @IBAction func createEvent(sender: AnyObject) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewControllerWithIdentifier("CreateEventViewController") as UIViewController
+//        self.presentViewController(vc, animated: true, completion: nil)
+        self.performSegueWithIdentifier("createEventNavigation", sender: self)
+    }
+    
+    @IBAction func makeEvent(sender: AnyObject){
+        let store = EKEventStore()
+        store.requestAccessToEntityType(EKEntityType.Event, completion: {(accessGranted:Bool, error: NSError?) in
+            if accessGranted == true {
+                let event = EKEvent(eventStore: store)
+                let eventName = self.eventTitle.text
+                event.title = eventName!
+                event.startDate = NSDate() //today
+                event.endDate = event.startDate.dateByAddingTimeInterval(60*60) //1 hour long meeting
+                event.calendar = store.defaultCalendarForNewEvents
+                //let err: NSError?
+                do {
+                    //try store.saveEvent(event, span: EKSpan.ThisEvent)
+                    try store.saveEvent(event, span: EKSpan.ThisEvent)
+                    print("Saved Event")
+                    dispatch_async(dispatch_get_main_queue(), {
+                    let alert: UIAlertView = UIAlertView(title: "Event Saved", message: "You have saved an event to your calendar!", delegate: nil, cancelButtonTitle: "Wahoowa")
+                    alert.show()
+                    })
+                } catch {
+                    // report error
+                }
+                
+                
+                //self.savedEventId = event.eventIdentifier //save event id to access this particular event later
+            } else {
+                print("Need to get permission")
+            }
+        })
+    }
 
+    
+//    @IBAction func createEvent(sender: AnyObject) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewControllerWithIdentifier("CreateEventViewController") as UIViewController
+//        self.presentViewController(vc, animated: true, completion: nil)
+//    }
     
 
     /*
